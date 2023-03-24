c6042ef4b8f99ce68d520761696e35cb8281658a
mcp <disku1986@gmail.com>
2023-03-24 16:04:22 +0200

    made computerChoice better

---
+++ rockpaperscisors.js
@@ -0,0 +1,30 @@
+
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3 );
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "scissors";
+            break;
+        case 2:
+            return "paper";
+            break;
+    };
+}console.log(getComputerChoice);
+    function playRound(playerSelection, computerSelection) {
+        
+       
+    
+    
+    const playerSelection = rock;
+    let result = playerSelection.toUpperCase();
+    const computerSelection = getComputerChoice();
+    return "You Lose! Paper beats Rock";
+    }
+    console.log(playRound(playerSelection, computerSelection));
+    game(playRound)= for (let i= 0 ; i < 5 ; i++) {
+        
+    }
+    
